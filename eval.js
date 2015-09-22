199b4b2c932de225af11c0995eb39af6ce7b6a7d
Corbin Page <corbin.page@gmail.com>
2015-09-22 19:28:03 -0400

    Working on evaluating conditions

--- eval.js
+++ eval.js
@@ -12,18 +12,20 @@
 var json = require("JSON");
 
 var conditionalOperators = ["AND", "OR"];
-var scalarOperators = ["=", ">", "<", ">=", "<=", "!=", "is null", "is not null"];
+var scalarOperators = ["===", ">", "<", ">=", "<=", "!=", "is null", "is not null"];
 var groupOperators = ["in", "not in"];
 
 operators = conditionalOperators.concat(scalarOperators.concat(groupOperators));
 
-function evalCondition(condition) {
-	console.log("Eval");
+function evalCondition(val,condition) {
+	console.log("-");
 
 	if(conditionalOperators.indexOf(condition.operator) != -1) {
 		console.log("Conditional");
 
-		var output = condition.references.map(evalCondition);
+		var output = condition.references.map(function(c) {
+			return evalCondition(val,c)
+		});
 
 		if(condition.operator === "AND") {
 			return output.indexOf(false) === -1
@@ -37,20 +39,19 @@ function evalCondition(condition) {
 		console.log("Group");
 	} else if (scalarOperators.indexOf(condition.operator) != -1) {
 		console.log("Scalar");
-		return eval("1 != 1");
+		var expression = val + " " + condition.operator + " " + condition.value
+		return eval(expression);
 	} else {
 		console.log("ERROR")
 		return false;
 	}	
-
-	console.log("---------");
 }
 
 var testCondition = {
-	operator: "=",
+	operator: "===",
 	references: [],
 	input: "score",
-	value: 1
+	value: 5
 };
 
 var testAndCondition = {
@@ -60,7 +61,7 @@ var testAndCondition = {
 	value: null
 };
 
-var decision = evalCondition(testAndCondition);
+var decision = evalCondition(5, testAndCondition);
 console.log("-------")
 console.log(decision)
 
